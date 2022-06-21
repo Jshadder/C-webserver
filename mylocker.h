5e5631aa43842aa93cf503fa5af7379016691762
Jshadder <104785081+Jshadder@users.noreply.github.com>
2022-06-22 00:57:48 +0800

    实现mylock封装类

--- mylocker.h
+++ mylocker.h
@@ -9,19 +9,20 @@
 class mysem{
 public:
     mysem(){
-
+        if(sem_init(&m_sem,0,0)!=0)
+            throw std::exception();
     }
 
     ~mysem(){
-
+        sem_destroy(&m_sem);
     }
 
     bool wait(){
-
+        return sem_wait(&m_sem)==0;
     }
 
     bool post(){
-
+        return sem_post(&m_sem)==0;
     }
 
 private:
@@ -31,19 +32,21 @@ private:
 class mylocker{
 public:
     mylocker(){
-
+        if(pthread_mutex_init(&m_mutex,nullptr)!=0){
+            throw std::exception();
+        }
     }
 
     ~mylocker(){
-
+        pthread_mutex_destroy(&m_mutex);
     }
 
     bool lock(){
-
+        return pthread_mutex_lock(&m_mutex)==0;
     }
 
     bool unlock(){
-
+        return pthread_mutex_unlock(&m_mutex)==0;
     }
 
 private:
@@ -52,23 +55,34 @@ private:
 
 class mycond{
 public:
-    cond(){
-
+    mycond(){
+        if(pthread_mutex_init(&m_mutex,nullptr)!=0)
+            throw std::exception();
+        if(pthread_cond_init(&m_cond,nullptr)!=0){
+            pthread_mutex_destroy(&m_mutex);
+            throw std::exception();
+        }
     }
 
-    ~cond(){
-
+    ~mycond(){
+        pthread_mutex_destroy(&m_mutex);
+        pthread_cond_destroy(&m_cond);
     }
 
     bool wait(){
-
+        assert(pthread_mutex_lock(&m_mutex)==0);
+        int ret=pthread_cond_wait(&m_cond,&m_mutex);
+        assert(pthread_mutex_unlock(&m_mutex)==0);
+        return ret==0;
     }
 
     bool signal(){
-
+        return pthread_cond_signal(&m_cond)==0;
     }
 
 private:
     pthread_mutex_t m_mutex;
     pthread_cond_t m_cond;
 };
+
+#endif
